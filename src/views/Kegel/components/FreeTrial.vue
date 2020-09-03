764d8e843055bf75fe0aa45f832e9bdbab17ce0a
Max <botkill1969@gmail.com>
2020-09-03 10:31:07 +0300

    Land

---
+++ src/views/Kegel/components/FreeTrial.vue
@@ -0,0 +1,13 @@
+<template>
+  $END$
+</template>
+
+<script>
+  export default {
+    name: "FreeTrial"
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/Kegel/components/WhatIs.vue
@@ -0,0 +1,13 @@
+<template>
+  $END$
+</template>
+
+<script>
+  export default {
+    name: "WhatIs"
+  }
+</script>
+
+<style scoped>
+
+</style>
